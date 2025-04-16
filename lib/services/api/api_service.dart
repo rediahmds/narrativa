36d75bf64d38a67fb4589236c2dfb83b9578fffa
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-04-16 19:57:06 +0700

    feat(services): :sparkles: http error parser

--- lib/services/api/api_service.dart
+++ lib/services/api/api_service.dart
@@ -18,6 +18,35 @@ class ApiService {
       AppPaths.login.path,
       data: payload.toJson(),
     );
-    return LoginResponse.fromJson(response.data);
+    return LoginResponse.fromJson(response.toString());
+  }
+
+  String parseDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+      case DioExceptionType.receiveTimeout:
+      case DioExceptionType.sendTimeout:
+        return "Connection timeout. Please try again.";
+
+      case DioExceptionType.badResponse:
+        final statusCode = dioException.response?.statusCode;
+        if (statusCode != null) {
+          final errorMessage =
+              dioException.response?.data['message'] ??
+              "Check your input and try again.";
+          return "Server error ($statusCode). $errorMessage";
+        }
+
+        return "Received invalid response from the server.";
+
+      case DioExceptionType.cancel:
+        return "Request cancelled.";
+
+      case DioExceptionType.connectionError:
+        return "No internet connection. Please check your network.";
+
+      default:
+        return "Unexpected HTTP error occurred. Please try again later.";
+    }
   }
 }

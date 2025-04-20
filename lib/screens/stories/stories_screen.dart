dde4a6065eeb81cff705ac3a2ea5851ae3411237
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-04-20 10:06:08 +0700

    feat(screens): :sparkles: story list

--- lib/screens/stories/stories_screen.dart
+++ lib/screens/stories/stories_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:narrativa/providers/providers.dart';
 import 'package:narrativa/static/static.dart';
+import 'package:narrativa/ui/ui.dart';
 import 'package:provider/provider.dart';
 
 class StoriesScreen extends StatefulWidget {
-  const StoriesScreen({super.key, required this.onLogout});
+  const StoriesScreen({
+    super.key,
+    required this.onLogout,
+    required this.onStoryTap,
+  });
 
   final Function onLogout;
+  final void Function(String) onStoryTap;
 
   @override
   State<StoriesScreen> createState() => _StoriesScreenState();
@@ -51,9 +57,9 @@ class _StoriesScreenState extends State<StoriesScreen> {
                 itemCount: stories.length,
                 itemBuilder: (context, index) {
                   final story = stories[index];
-                  return ListTile(
-                    title: Text(story.name),
-                    subtitle: Text(story.description),
+                  return StoryCardWidget(
+                    story: story,
+                    onTap: () => widget.onStoryTap(story.id),
                   );
                 },
               );

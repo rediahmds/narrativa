1787c01f8f72b1016052c9155b7590377edb66e7
rediahmds_fedora.ws <redi.ngoding@gmail.com>
2025-04-23 17:13:45 +0700

    feat(screens): :sparkles: add form

--- lib/screens/add/add_story_screen.dart
+++ lib/screens/add/add_story_screen.dart
@@ -1,14 +1,119 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class AddStoryScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:narrativa/ui/ui.dart';
+import 'package:provider/provider.dart';
+import 'package:narrativa/providers/providers.dart';
+
+class AddStoryScreen extends StatefulWidget {
   const AddStoryScreen({super.key});
+
+  @override
+  State<AddStoryScreen> createState() => _AddStoryScreenState();
+}
+
+class _AddStoryScreenState extends State<AddStoryScreen> {
+  final _descriptionController = TextEditingController();
+
+  @override
+  void dispose() {
+    _descriptionController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Add Story"), centerTitle: true),
-      body: const Center(
-        child: Text("Add Story Screen", style: TextStyle(fontSize: 24)),
+      floatingActionButton: Container(
+        margin: const EdgeInsets.only(left: 12, right: 12),
+        width: MediaQuery.of(context).size.width,
+        child: FilledButton.icon(
+          icon: const Icon(Icons.send_rounded),
+          onPressed: () {},
+          label: const Text("Submit"),
+        ),
+      ),
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Consumer<AddStoryProvider>(
+            builder: (_, addStoryProvider, _) {
+              return Padding(
+                padding: const EdgeInsets.all(18),
+                child: Column(
+                  spacing: 18,
+                  children: [
+                    if (addStoryProvider.imagePath != null) ...[
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Theme.of(context).colorScheme.outlineVariant,
+                          ),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: Image.file(
+                            File(addStoryProvider.imagePath!),
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                    ] else ...[
+                      const Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          spacing: 8,
+                          children: [
+                            Icon(Icons.image, size: 100),
+                            Text("No image selected"),
+                          ],
+                        ),
+                      ),
+                    ],
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            addStoryProvider.openGalleryView();
+                          },
+                          child: const Text("Select from Gallery"),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            addStoryProvider.openCameraView();
+                          },
+                          child: const Text("Take a Photo"),
+                        ),
+                      ],
+                    ),
+                    ConstrainedBox(
+                      constraints: const BoxConstraints(maxHeight: 200),
+                      child: NarrativaTextField(
+                        controller: _descriptionController,
+                        labelText: "Description",
+                        textInputType: TextInputType.multiline,
+                        expands: true,
+                        hintText: "Write a description",
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        validator: (description) {
+                          if (description == null || description.isEmpty) {
+                            return "Description cannot be empty";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 18),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
